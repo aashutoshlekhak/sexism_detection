237ece165a322c3a690fbd99c90066d20c2050ae
fuseMuskan <bmuskan007@gmail.com>
2024-08-19 21:00:13 +0545

    bugfix: fixed the preprocessing script to work with csmb dataset also

--- src/stages/preprocess_data.py
+++ src/stages/preprocess_data.py
@@ -1,11 +1,11 @@
 import os
 import sys
 import re
+import argparse
 import pandas as pd
+from loguru import logger
 sys.path.insert(0, os.getcwd())
 from src.utils.utils import load_yaml
-from loguru import logger
-import argparse
 
 def preprocess_text(text:str)->str:
     """Preprocess text by replacing user name with [USER], links with [LINK] and removing hash tags (#)
@@ -26,16 +26,21 @@ def preprocess_text(text:str)->str:
     return text
 
 
-def preprocess_data(config_path:str)->None:
+def preprocess_data(config_path:str, data:str)->None:
     """Preprocess the dataset text column
 
     Args:
         config_path (str): path to config file (eg:params.yaml)
+        data (str): the dataset i.e exist or csmb
+
     """
     config = load_yaml(config_path)
-    raw_data_path = config["data"]["exist"]["raw"]
-    preprocess_data_path = config["data"]["exist"]["preprocessed"]
-    df = pd.read_csv(raw_data_path, delimiter="\t")
+    raw_data_path = config["data"][data]["raw"]
+    preprocess_data_path = config["data"][data]["preprocessed"]
+    if raw_data_path.split(".")[-1] == "tsv":
+        df = pd.read_csv(raw_data_path, delimiter="\t")
+    else:
+        df = pd.read_csv(raw_data_path)
     logger.info(f"READ {raw_data_path} \n with shape {df.shape}")
     df['text'] = df['text'].apply(preprocess_text)
     logger.info("Data Preprocessing Completed")
@@ -45,5 +50,6 @@ def preprocess_data(config_path:str)->None:
 if __name__=="__main__":
     arg_parser = argparse.ArgumentParser()
     arg_parser.add_argument("--config", dest="config", required=True)
+    arg_parser.add_argument("--data",dest="data", required=True)
     args = arg_parser.parse_args()
-    preprocess_data(args.config)
+    preprocess_data(args.config, args.data)
