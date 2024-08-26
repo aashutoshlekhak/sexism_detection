7e53809efcfbc794f81a89b29104e812fc92ff5f
fuseMuskan <bmuskan007@gmail.com>
2024-08-26 11:56:17 +0545

    fix: fixed the hardcoded labels

--- src/stages/evaluate.py
+++ src/stages/evaluate.py
@@ -14,8 +14,6 @@ from tqdm.auto import tqdm
 sys.path.insert(0, os.getcwd())
 from src.utils.utils import load_yaml
 
-## TODO: 1. Map class labels to prediction label
-        # 2. Save the reports in proper directory 
 
 
 def load_model(model_path: str, num_labels:int):
@@ -33,7 +31,7 @@ def load_model(model_path: str, num_labels:int):
 
     return pipe
 
-def evaluate(config_path:str, data_name:str, data_type:str, model_ckpt:str, fold:int, task:Optional[str])->None:
+def evaluate(config_path:str, data_name:str, data_type:str, model_ckpt:str,model_name:str,fold:int, task:Optional[str])->None:
     config = load_yaml(config_path)
     save_path = config["data"]["test"][data_name][data_type]
     dataset_path = os.path.join(os.path.dirname(save_path), os.path.basename(save_path).split(".")[0])
@@ -66,19 +64,19 @@ def evaluate(config_path:str, data_name:str, data_type:str, model_ckpt:str, fold
     # Use KeyDataset and tqdm for generating predictions
     for out in tqdm(pipe(KeyDataset(test_data, "text"), batch_size=16, truncation=True), total=len(test_data)):
         predicted_label = out['label']
-        predicted_label_index = int(predicted_label.split()[-1])
+        predicted_label_index = int(predicted_label.split("_")[-1])
         predictions.append(predicted_label_index)
     
 
     if task:
-        metric_dir = os.path.join("src/metrics", model_ckpt, data_type,data_name,task,str(fold))
+        metric_dir = os.path.join("src/metrics", model_name, data_type,data_name,task, f"fold_{fold}")
     else:
-        metric_dir = os.path.join("src/metrics", model_ckpt, data_type,data_name, str(fold))
+        metric_dir = os.path.join("src/metrics", model_name, data_type,data_name, f"fold_{fold}")
 
     os.makedirs(metric_dir, exist_ok=True)
 
     # Generate classification report
-    report = classification_report(labels, predictions)
+    report = classification_report(labels, predictions,target_names=class_labels)
 
     with open(f"{metric_dir}/classification_report.txt", 'w') as f:
         f.write(report)
@@ -88,7 +86,7 @@ def evaluate(config_path:str, data_name:str, data_type:str, model_ckpt:str, fold
 
     # Plot confusion matrix using seaborn
     plt.figure(figsize=(6, 4))
-    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False, xticklabels=['non-sexist', 'sexist'], yticklabels=['non-sexist', 'sexist'])
+    sns.heatmap(cm, annot=True, fmt='d', cmap='Blues', cbar=False, xticklabels=class_labels, yticklabels=class_labels)
     plt.title('Confusion Matrix')
     plt.xlabel('Predicted Labels')
     plt.ylabel('True Labels')
@@ -104,8 +102,9 @@ if __name__ == "__main__":
     arg_parser.add_argument("--data-name",dest="data_name", required=True)
     arg_parser.add_argument("--data-type",dest="data_type", required=True)
     arg_parser.add_argument("--model-path",dest="model_path", required=True)
+    arg_parser.add_argument("--model-name",dest="model", required=True)
     arg_parser.add_argument("--fold",dest="fold", required=True)
     arg_parser.add_argument("--task",dest="task", required=False)
 
     args = arg_parser.parse_args()
-    evaluate(args.config, args.data_name, args.data_type,args.model, int(args.fold),args.task) 
+    evaluate(args.config, args.data_name, args.data_type,args.model_path,args.model,int(args.fold),args.task) 
