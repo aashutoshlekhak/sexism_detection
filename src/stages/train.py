e38eade35726baeaf5999fd45e043f70b465b864
fuseMuskan <bmuskan007@gmail.com>
2024-08-27 09:07:28 +0545

    bugfix

--- src/stages/train.py
+++ src/stages/train.py
@@ -74,7 +74,7 @@ def train(config_path:str, data_name:str, data_type:str, model_name:str, fold:in
         output_dir = os.path.join("models", model_name, data_type,data_name,f"fold_{fold}")
 
     os.makedirs(output_dir, exist_ok=True)
-    output_dir = os.makedirs(os.path.join("models", f"{model_name}/{data_type}/{fold}")) # models/distil-bert/raw/fold/
+    output_dir = os.makedirs(os.path.join("models", f"{model_name}/{data_type}/{fold}"), exist_ok=True) # models/distil-bert/raw/fold/
     model_name = f"{output_dir}/{model_ckpt}"
 
     training_args = TrainingArguments(output_dir=model_name,
