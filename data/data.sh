fc6d865a9f61ada29aa3af3cc4fe2e736a2981ef
fuseMuskan <bmuskan007@gmail.com>
2024-08-27 10:33:36 +0545

    added gdown and script to download dataset

--- data/data.sh
+++ data/data.sh
@@ -1,10 +1,17 @@
 # Setup data directory
 mkdir -p data/raw/exist
+cd data/raw/exist
+echo "Downloading exist dataset"
+gdown https://drive.google.com/uc?id=1-5L3Q_qy6lpOQUDrNM9q_4q0wwGR-zFr
+gdown https://drive.google.com/uc?id=1-C5K9x2-rQAAi7wRwt4aMqkW8qcntLqC
+
+cd ../../.. 
 mkdir -p data/raw/csmb
+cd data/raw/csmb
+echo "Downloading csmb dataset"
+gdown https://drive.google.com/uc?id=1jfE609X87uqpUWMLiJq_HN7Y7BVLlO_c
 mkdir -p data/preprocessed/exist
 mkdir -p data/preprocessed/csmb
 mkdir -p data/augmented/exist
 mkdir -p data/augmented/csmb
 
-# Download the data files and put it in respective folders (raw and preprocessed). The augmented dataset can be generated
-# using src/stages/augment_data.py
